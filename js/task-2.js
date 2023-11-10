d4c92c57621fddd8ff5fc4219992d59db9a2fffa
JuliaDzimina <132845748+JuliaDzimina@users.noreply.github.com>
2023-11-10 21:42:28 +0200

    task01

--- js/task-2.js
+++ js/task-2.js
@@ -1,10 +1,27 @@
-function getShippingMessage(country, price, deliveryFee){
-    const totalPrice = price + deliveryFee;
-    const message = `Shipping to ${country} will cost ${totalPrice} credits`;
-    return message;
+// Задача 2. Форматування повідомлення
+// Оголоси функцію formatMessage(message, maxLength), яка приймає рядок (параметр message) та перевіряє його довжину відповідно до заданої максимальної довжини (параметр maxLength).
+
+// Доповни код функції таким чином, що:
+
+// Якщо довжина рядка дорівнює або менша за maxLength, то функція повертає початковий рядок без змін.
+// Якщо довжина перевищує maxLength, то функція обрізає рядок до maxLength символів, додає трикрапку "..." в кінці та повертає обрізану версію.
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
+
+function formatMessage(message, maxLength){
+    const messageLength = message.length;
+    if(messageLength <= maxLength){
+        return message;
+    } else {
+        
+    }
+
 
 }
 
-console.log(getShippingMessage("Australia", 120, 50)); 
-console.log(getShippingMessage("Germany", 80, 20)); 
-console.log(getShippingMessage("Sweden", 100, 20))
+
+console.log(formatMessage("Curabitur ligula sapien", 16)); 
+console.log(formatMessage("Curabitur ligula sapien", 23)); 
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); 
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); 
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); 
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); 
